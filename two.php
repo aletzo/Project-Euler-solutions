0c469a335ae3b63592307e52a11e642c2edac035
ale tzo <ajo@mydevnull.net>
2012-03-02 20:34:30 +0200

    fixed the two.php code

--- two.php
+++ two.php
@@ -5,27 +5,18 @@ $sum = 2;
 $prev1 = 2;
 $prev2 = 1;
 
-
 for ($i = 0; $i < 100; $i++) {
     if ($prev2 > 4000000) {
         break;
     }
-
 
     $fib = $prev1 + $prev2;
     $prev2 = $prev1;
     $prev1 = $fib;
 
     if ($fib % 2 == 0) {
-        echo $fib . '<br>';
-        
         $sum += $fib;
     }
-
-
 }
 
-echo '<br>===========<br>';
-echo $fib;
-echo '<br />';
 echo $sum;
